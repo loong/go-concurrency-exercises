6af71be2d9cd18d6e19b9a4495a8e6db7d79d098
Vinayak Shinde <vinayak.shinde@cerebras.net>
2025-05-25 23:35:27 -0700

    First attempt at solving problems

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,11 +12,12 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, throttle *time.Ticker) {
 	defer wg.Done()
 
 	if depth <= 0 {
@@ -35,7 +36,8 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		<-throttle.C
+		go Crawl(u, depth-1, wg, throttle)
 	}
 }
 
@@ -43,6 +45,7 @@ func main() {
 	var wg sync.WaitGroup
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	throttle := time.NewTicker(2 * time.Second)
+	Crawl("http://golang.org/", 4, &wg, throttle)
 	wg.Wait()
 }
--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,8 @@
 
 package main
 
+import "time"
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,8 +23,19 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+
+	timer := time.NewTimer(10 * time.Second)
+	done := make(chan bool)
+	go func() {
+		process()
+		done <- true
+	}()
+	select {
+	case <-timer.C:
+		return false
+	case <-done:
+		return true
+	}
 }
 
 func main() {
